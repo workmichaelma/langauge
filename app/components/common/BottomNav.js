565a019175a2a011c23d0312e71a103f7bbb79c8
Michael Ma <michael.ma@scmp.com>
2021-01-01 05:45:53 +0800

    Basic setup

---
+++ app/components/common/BottomNav.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import ReorderIcon from '@material-ui/icons/Reorder';
+import RestoreIcon from '@material-ui/icons/Restore';
+import QueueIcon from '@material-ui/icons/Queue';
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+import map from 'lodash/map'
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: '100%',
+  }
+});
+
+const routes = [
+  {
+    icon: <ReorderIcon />,
+    pathname: '/',
+  },
+  {
+    icon: <RestoreIcon />,
+    pathname: '/favorite',
+  },
+  {
+    icon: <QueueIcon />,
+    pathname: '/add',
+  }
+]
+
+export default function SimpleBottomNavigation() {
+  const classes = useStyles();
+  const router = useRouter();
+
+  console.log({router})
+
+  return (
+    <BottomNavigation
+      showLabels
+      className={classes.root}
+    >
+      {
+        map(routes, (r, key) => {
+          return (
+            <Link href={r.pathname} key={key}>
+              <BottomNavigationAction {...{ icon: r.icon, selected: r.pathname === router.pathname }} />
+            </Link>
+          )
+        })
+      }
+    </BottomNavigation>
+  );
+}
---
+++ app/pages/_app.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../src/theme';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
+import Container from '@material-ui/core/Container';
+import BottomNav from 'components/common/BottomNav'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    position: 'fixed',
+    bottom: 0,
+    left: 0,
+    width: '100%',
+    zIndex: 100,
+    background: 'white',
+  },
+  footerPadding: {
+    padding: theme.spacing(1, 1),
+  }
+}));
+
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+  const classes = useStyles();
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <div className={classes.root}>
+          <CssBaseline />
+
+          <Container component="main" className={classes.main} maxWidth="xs">
+            <Component {...pageProps} />
+          </Container>
+
+          <footer className={classes.footer}>
+            <Divider />
+            <div className={classes.footerPadding}>
+              <Container maxWidth="sm">
+                <BottomNav />
+              </Container>
+            </div>
+          </footer>
+        </div>
+      </ThemeProvider>
+    </React.Fragment>
+  );
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
